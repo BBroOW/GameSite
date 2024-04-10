75a066014a5b061cfdf518dc26e5047a77a1d177
BBroOW <brage_hagelund@outlook.com>
2024-04-10 12:48:09 +0200

    Add react-router-dom dependency and update layout component

--- GameSite/src/layout/index.tsx
+++ GameSite/src/layout/index.tsx
@@ -4,7 +4,9 @@ import "../index.css";
 const Layout: React.FC = () => {
   return (
     <div>
-      <div className="w-full h-24 bg-slate-700 text-white">hei</div>
+      <div className="w-full h-24 bg-slate-700 text-white">
+        <p>GMAING</p>
+      </div>
       <Outlet />
     </div>
   );
